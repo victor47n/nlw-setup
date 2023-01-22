189186cbb134b5abdf1fd69ebde8a208f94f2964
Victor Hugo da Silva <victor47n@gmail.com>
2023-01-22 00:36:17 -0300

    Aula 4 finalizada

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefinied;
+      new: undefinied;
+      habit: {
+        date: string;
+      }
+    }
+  }
+}
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { View, Text, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates'
 
@@ -11,6 +12,8 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOdDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -38,6 +41,7 @@ export function Home() {
             datesFromYearStart.map(date => (
               <HabitDay
                 key={date.toISOString()}
+                onPress={() => navigate('habit', { date: date.toISOString() })}
               />
             ))
           }
